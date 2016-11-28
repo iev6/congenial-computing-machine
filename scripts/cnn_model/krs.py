d249fe770063b358db64df2c6eddadc74e3b1ca7
iev6 <giridhursriram@gmail.com>
2016-11-28 15:23:23 +0530

    Added scripts for extending number of speakers Mean subtracted in the CNN model.

--- scripts/cnn_model/krs.py
+++ scripts/cnn_model/krs.py
@@ -30,26 +30,27 @@ with open('test_data.pkl','rb') as f1:
 
 
 batch_size = 128
-nb_classes = 10
-nb_epoch = 12
+nb_classes = 38
+nb_epoch = 50
 
 # input image dimensions
 img_rows, img_cols = 128, 100
 
-train_label = np_utils.to_categorical(train[:,-1]-1,nb_classes=4)
+train_label = np_utils.to_categorical(train[:,-1]-1,nb_classes=38) #HACK : nb_classes hardcoded!!!
 train = train[:,:-1]
-(trX,teX,trY,teY) = train_test_split(train,train_label,test_size=0.1)
+train = train - np.mean(train,axis=0)
+(trX,teX,trY,teY) = train_test_split(train,train_label,test_size=0.2)
 trX = trX.reshape(trX.shape[0],img_rows,img_cols,1)
 teX = teX.reshape(teX.shape[0],img_rows,img_cols,1)
 
 # number of convolutional filters to use
-n_s = 4
-nb_filters1 = 32
-nb_filters2 = 64
+n_s = nb_classes
+nb_filters1 = 64
+nb_filters2 = 128
 # size of pooling area for max pooling
 pool_size = (4, 4)
 # convolution kernel size
-kernel_size = (3, 3)
+kernel_size = (5, 5) ##EDIT
 stride = (2,2) #2 steps in both directions
 input_shape = (img_rows, img_cols, 1)
 model = Sequential()
@@ -82,11 +83,11 @@ model.add(Activation('relu'))
 model.add(Dense(n_s))
 model.add(Activation('softmax'))
 #Optimizer
-optim = Nadam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-04, schedule_decay=0.0001)
+optim = Nadam(lr=1e-3, beta_1=0.9, beta_2=0.999, epsilon=1e-06, schedule_decay=0.0001)
 model.compile(loss='categorical_crossentropy',optimizer=optim,metrics=['accuracy'])
 
 #Training
-model.fit(trX, trY, batch_size=5, nb_epoch=10*nb_epoch,
+model.fit(trX, trY, batch_size=128, nb_epoch=100,
           verbose=1, validation_data=(teX, teY))
 #Testing
 score = model.evaluate(X_test, Y_test, verbose=0)
--- scripts/utils/spec.py
+++ scripts/utils/spec.py
@@ -11,13 +11,17 @@ import librosa.feature
 import glob
 import glob2
 import shutil
-import cPickle
+import cPickle as pkl
 import re
 
 def path_info(audio_path):
     (dir_name,file_name) = os.path.split(audio_path)
     (dir_name,speaker_name) = os.path.split(dir_name)
     return file_name,speaker_name
+
+def im2double(im):
+    info = np.iinfo(im.dtype) # Get the data type of the input image
+    return im.astype(np.float) / info.max # Divide all values by the largest possible value in the datatype
 
 def specgram(audio_path='test.wav',params=None):
 
@@ -54,7 +58,44 @@ def specgram(audio_path='test.wav',params=None):
 path_to_timit = "../../datasets/timit/"  #HACK:0 PATH BEWARE!! #TODO:0 FIX PATH ERRORS
 train_dialect = "train/dr1/"
 test_dialect = "test/dr1/"
-speakers = {"fcjf0":1,"fdaw0":2,"fdml0":3,"fecd0":4}
+speakers = {'fcjf0': 21,
+ 'fdaw0': 28,
+ 'fdml0': 33,
+ 'fecd0': 5,
+ 'fetb0': 9,
+ 'fjsp0': 36,
+ 'fkfb0': 23,
+ 'fmem0': 27,
+ 'fsah0': 25,
+ 'fsjk1': 26,
+ 'fsma0': 34,
+ 'ftbr0': 2,
+ 'fvfb0': 14,
+ 'fvmh0': 18,
+ 'mcpm0': 17,
+ 'mdac0': 19,
+ 'mdpk0': 16,
+ 'medr0': 38,
+ 'mgrl0': 22,
+ 'mjeb1': 29,
+ 'mjwt0': 30,
+ 'mkls0': 35,
+ 'mklw0': 31,
+ 'mmgg0': 12,
+ 'mmrp0': 7,
+ 'mpgh0': 3,
+ 'mpgr0': 11,
+ 'mpsw0': 10,
+ 'mrai0': 6,
+ 'mrcg0': 15,
+ 'mrdd0': 20,
+ 'mrso0': 1,
+ 'mrws0': 8,
+ 'mtjs0': 13,
+ 'mtpf0': 4,
+ 'mtrr0': 32,
+ 'mwad0': 37,
+ 'mwar0': 24}
 
 #Creating the datasets
 for speaker in speakers.keys():
@@ -67,7 +108,7 @@ for speaker in speakers.keys():
 train_set = glob2.glob(path_to_timit+"**/*.png")
 for fil in train_set:
     f_name,speaker_name = path_info(fil)
-    destn = '../../datasets/train_set/'+speaker_name+'/'
+    destn = '../../datasets/train_set/'+speaker_name+'_'+f_name
     shutil.copy2(src=fil,dst=destn)
 #making a pickle out of all these pictures in ../datasets/train_set
 train_set = glob2.glob('../../datasets/train_set/*.png')
@@ -77,9 +118,11 @@ for image in train_set:
     (file_name,temp) = path_info(image)
     m1 = p1.match(string=file_name)
     (speaker_name,temp) = m1.groups(0)
-    img = scipy.misc.imread(name=image,flatten=True)
+    img = (scipy.misc.imread(name=image,flatten=True))/255.0
     img = np.reshape(a=img,newshape=(1,-1))
     spkr = speakers[speaker_name]
     img = np.append(img,spkr)
-    train = np.vstack((train,img)
+    train = np.vstack((train,img))
 train  = train[1:,:]
+with open('../cnn_model/train_data.pkl','wb') as f1:
+    pkl.dump(train,f1)
